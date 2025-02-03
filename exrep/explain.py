7c6c464aaaaa8d92badfb2ff02988e7c91b05a4e
nhatkhtn <hoangx@ufl.edu>
2025-02-02 21:24:32 -0500

    Refactor

--- exrep/explain.py
+++ exrep/explain.py
@@ -1,9 +1,106 @@
-import torch
+from typing import Literal
 
-def train_local_model(
-    inputs: torch.Tensor,
-    targets: torch.Tensor,
-    model_args: dict,
-    device,
-):
+from PIL import Image
+import numpy as np
+import torch
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+
+class LocalFeatureManager:
+    """Manages the local feature space and enable sampling of mixed images.
+
+    Note: there are four spaces of inputs:
+    1. feature space: the space of local features (i.e, id of the segments)
+    2. ordinal space: the space of integers from (0, 1, ..., num_used_features - 1)
+    3. categorical space: the space of matrices, where each row has length num_used_features, and each value is one of choices
+    4. one-hot space: the space of one-hot encoded categorical matrices
+    """
+    def __init__(self, input_image: Image.Image, input_segments: np.ndarray):
+        self.input_image = input_image
+        self.input_segments = input_segments
+        self.im_shape = np.array(input_image).shape
+        self.ordinal_encoder = LabelEncoder()
+        self.onehot_encoder = OneHotEncoder(sparse_output=False, drop='first')
+        self.choices_per_feature = None
+        self.binary_masks = None
+        self.replacements = None
+
+    def fit(self, 
+        choices_per_feature: dict[int, list[int]], 
+        replacements: torch.Tensor, 
+        binary_masks: torch.Tensor,
+    ):
+        assert binary_masks.shape[0] == len(replacements), "Number of masks and crops must match"
+
+        self.ordinal_encoder.fit(list(choices_per_feature.keys()))
+        self.choices_per_feature = choices_per_feature
+        self.binary_masks = binary_masks
+        self.replacements = replacements
+
+    def sample_data(self, num_samples: int):
+        """Returns two tensors, the one-hot encoded categorical features and the mixed images for regression training."""
+        categorical_traits = self.sample_features(num_samples)
+        self.categorical_traits = categorical_traits    # TODO: remove
+        self.onehot_encoder.fit(categorical_traits)
+        onehot_traits = self.onehot_encoder.transform(categorical_traits)
+        images = self.mix(categorical_traits)
+        return torch.tensor(onehot_traits, dtype=torch.float32), images
+
+    def sample_features(self, num_samples: int):
+        """Returns a matrix containing categorical feature vectors."""
+        rng = np.random.default_rng(seed=42)
         
+        num_features = len(self.choices_per_feature)
+        features = np.zeros((num_samples, num_features), dtype=np.int32)
+        for f, choices in self.choices_per_feature.items():
+            i = self.ordinal_encoder.transform([f])[0]
+            features[:, i] = rng.choice(choices, num_samples)
+
+        return torch.tensor(features, dtype=torch.int64)
+        
+    def mix(self, features: torch.Tensor) -> np.ndarray:
+        """Given features (a list of vectors in categorical space), return the mixed images (i.e, the images created by 
+        overlaying the replacements according to the binary masks).
+        """
+        num_replacements = self.binary_masks.shape[0]
+        num_samples = features.shape[0]
+
+        indexed_masks = self.binary_masks * torch.arange(num_replacements)[:, None, None]
+        blended_masks = torch.take_along_dim(indexed_masks[None, :], features[..., None, None], dim=1).max(dim=1).values
+        images = torch.take_along_dim(
+            self.replacements.reshape(1, num_replacements, *self.im_shape), 
+            blended_masks.reshape(num_samples, 1, *self.im_shape[:2], 1), dim=1,
+        ).squeeze(dim=1)
+        return images.numpy()
+
+    def decode(self, x: np.ndarray, out_level: Literal["image", "feature", "ordinal", "categorical"], with_choices=False):
+        """Given a matrix x, return the decoded tensor at the specified level."""
+        assert out_level in ["image", "feature", "ordinal", "categorical"]
+
+        x = self.onehot_encoder.inverse_transform(x)
+        if out_level == "categorical": return x
+        if out_level == "image": 
+            return self.mix(torch.tensor(x, dtype=torch.int64))
+        
+        base_choices = np.zeros((len(self.choices_per_feature),), dtype=np.int32)
+        for i, (_, choices) in enumerate(self.choices_per_feature.items()):
+            # we assume the first choice is always the base choice
+            base_choices[i] = choices[0]
+
+        sig_features = [np.where(row != base_choices)[0] for row in x]
+        sig_with_choices = [
+            [(diff_index, row[diff_index]) for diff_index in diff_indices]
+            for diff_indices, row in zip(sig_features, x)
+        ]
+
+        if out_level == "ordinal": 
+            if not with_choices: return sig_features
+            return sig_with_choices
+
+        sig_with_choices = [
+            [(self.ordinal_encoder.inverse_transform(f), v) for f, v in row]
+            for row in sig_with_choices
+        ]
+        # out_level == "feature"
+        if not with_choices: return [[f for f, _ in row] for row in sig_with_choices]
+        return sig_with_choices
--- exrep/utils.py
+++ exrep/utils.py
@@ -6,8 +6,7 @@ from PIL import Image
 import numpy as np
 import torch
 from transformers.tokenization_utils_base import BatchEncoding
-
-from exrep.kd import KDLossNaive, distill_one_epoch
+from transformers import pipeline
 
 TensorDict = dict[str, torch.Tensor] | BatchEncoding
 
@@ -79,222 +78,41 @@ class Embedder:
             if normalize:
                 embeddings = torch.nn.functional.normalize(embeddings, p=2, dim=1)
             return embeddings
-
-class KDRegressor:
-    def __init__(self, 
-        n_output_dim: int,
-        temp_student: float = 1.0,
-        temp_teacher: float = 1.0,
-        num_epochs: int = 10,
-        batch_size: int = 256,
-        device: str = "cuda",
-    ):
-        self.n_output_dim = n_output_dim
-        self.temp_student = temp_student
-        self.temp_teacher = temp_teacher
-        self.num_epochs = num_epochs
-        self.batch_size = batch_size
-        self.device = device
-        self.logs = []
-        self.query_encoder = None
-        self.key_encoder = None
-        self.keys = None
-
-    def set_keys(self, keys: np.ndarray):
-        self.keys = torch.tensor(keys, device=self.device, dtype=torch.float32)
-
-    def fit(self, data: np.ndarray, embeddings: np.ndarray, sample_weight: np.ndarray):
-        """Fit the regressor to the (query) data.
-
-        Args:
-            data (np.ndarray): NumPy array of shape (n_samples, n_patches) containing the data with local features.
-            embeddings (np.ndarray): NumPy array of shape (n_samples, n_repr_dim) containing the embedded data (representations).
-            sample_weights (np.ndarray): NumPy array of shape (n_samples,) containing the sample weights.
-        """
-        # get the constants
-        n_samples, n_local_features = data.shape
-        n_repr_dim = embeddings.shape[1]
-        n_output_dim = self.n_output_dim
-        assert embeddings.shape[0] == n_samples, "Number of samples in data and embeddings must match."
-        assert self.keys is not None, "Keys must be set before fitting the model."
-
-        # create models
-        # student model (e')
-        self.query_encoder = torch.nn.Linear(n_local_features, n_output_dim).to(self.device)
-        # projector (r)
-        self.key_encoder = torch.nn.Linear(n_repr_dim, n_output_dim).to(self.device)
-
-        # create optimizer
-        params = list(self.query_encoder.parameters()) + list(self.key_encoder.parameters())
-        optimizer = torch.optim.AdamW(params, lr=1e-4, weight_decay=1e-1)
-
-        # create loss
-        sample_weight = torch.tensor(sample_weight)
-        # use fixed sample weight for now
-        sample_weight = torch.ones_like(sample_weight)
-        logger.warning("Using fixed sample weight for now.")
-        loss = KDLossNaive(
-            data_size=n_samples, gamma1=0.9, gamma2=0.9,
-            weights=sample_weight,
-            temp_student=self.temp_student, temp_teacher=self.temp_teacher,
-        ).to(self.device)
-
-        # load data
-        indices = torch.arange(n_samples)
-        dataset = torch.utils.data.StackDataset(images=torch.Tensor(data), indices=indices)
-        dataloader = torch.utils.data.DataLoader(dataset, batch_size=self.batch_size, shuffle=True)
-
-        # distill
-        for epoch in range(self.num_epochs):
-            logs = distill_one_epoch(
-                self.query_encoder, self.key_encoder, 
-                embeddings, self.keys,
-                dataloader, loss, optimizer, epoch, self.device, 
-                log_every_n_steps=5,
-            )
-            self.logs.extend(logs)
-
-        # intercept_ is (n_features,)
-        # self.intercept_ = self.query_encoder.bias.detach().cpu().numpy()
-        self.intercept_ = None
-        # coef_ is (n_features, n_patches)
-        # self.coef_ = self.query_encoder.weight.detach().cpu().numpy()
-        self.coef_ = []
         
-    def score(self, X, y, sample_weight=None):
-        logging.warning("Score is not implemented.")
-        return None
-        # y_pred = self.predict(X)
-        # return np.linalg.norm(y - y_pred, axis=1)
-
-    def predict(self, x: np.ndarray) -> np.ndarray:
-        logger.warning("Predict is not implemented.")
-        return None
-        # assert x.ndim == 2
-        # # x is (n_samples, n_patches)
-        # return x @ self.coef_.T + self.intercept_
-    
-    def embed(self, x: torch.Tensor) -> torch.Tensor:
-        """Embeds the input data with the query encoder."""
-        return self.query_encoder(x)
-    
-    def project(self, x: np.ndarray) -> np.ndarray:
-        """Projects an embedding to the output space with the key encoder."""
-        with torch.inference_mode():
-            x = torch.tensor(x, device=self.device, dtype=torch.float32)
-            return self.key_encoder(x).detach().cpu().numpy()
-
-class LocalRepresentationFitter:
-    def __init__(self,
-        n_output_dim: int,
-        temp_student: float = 1.0,
-        temp_teacher: float = 1.0,
-        alpha: float = 0.0,
-        num_epochs: int = 10,
-        batch_size: int = 256,
-        device: str = "cuda",
-    ):
-        self.n_output_dim = n_output_dim
-        self.temp_student = temp_student
-        self.temp_teacher = temp_teacher
-        self.alpha = alpha
-        self.num_epochs = num_epochs
-        self.batch_size = batch_size
-        self.device = device
-        self.logs = []
-        self.query_encoder = None
-        self.key_encoder = None
-
-    def fit(self, inputs: np.ndarray, targets: np.ndarray, keys: np.ndarray, sample_weight: np.ndarray, feature_groups: Sequence[Sequence[int]]):
-        """Fit the regressor to the (query) data.
-
-        Args:
-            inputs (np.ndarray): NumPy array of shape (n_samples, n_features) containing the data with local features.
-            targets (np.ndarray): NumPy array of shape (n_samples, n_repr_dim) containing the embedded data (representations).
-            keys (np.ndarray): NumPy array of shape (n_samples, n_output_dim) containing the keys.
-            sample_weights (np.ndarray): NumPy array of shape (n_samples,) containing the sample weights.
-            feature_groups (Sequence[Sequence[int]]): Sequence of sequences containing the feature groups, used for regularization.
-        """
-        # get the constants
-        n_samples, n_local_features = inputs.shape
-        n_repr_dim = targets.shape[1]
-        n_output_dim = self.n_output_dim
-        assert targets.shape[0] == n_samples, "Number of samples in inputs and targets must match."
-
-        # create models
-        # student model (e')
-        self.query_encoder = torch.nn.Linear(n_local_features, n_output_dim).to(self.device)
-        # projector (r)
-        self.key_encoder = torch.nn.Linear(n_repr_dim, n_output_dim).to(self.device)
-
-        # create optimizer
-        params = list(self.query_encoder.parameters()) + list(self.key_encoder.parameters())
-        optimizer = torch.optim.AdamW(params, lr=1e-4, weight_decay=1e-1)
-
-        sample_weight = torch.tensor(sample_weight)
-        # use fixed sample weight for now
-        sample_weight = torch.ones_like(sample_weight)
-
-        # create loss
-        logger.warning("Using fixed sample weight for now.")
-        loss = KDLossNaive(
-            data_size=n_samples, gamma1=0.9, gamma2=0.9,
-            weights=sample_weight,
-            temp_student=self.temp_student, temp_teacher=self.temp_teacher,
-        ).to(self.device)
-        def regularizer(layer):
-            return self.alpha * torch.sum(torch.stack([torch.linalg.vector_norm(layer.weight[:, group]) for group in feature_groups]))
-
-        # load data
-        indices = torch.arange(n_samples)
-        dataset = torch.utils.data.StackDataset(images=torch.Tensor(inputs), indices=indices)
-        dataloader = torch.utils.data.DataLoader(dataset, batch_size=self.batch_size, shuffle=True)
-        keys = torch.tensor(keys, device=self.device, dtype=torch.float32)
-
-        # distill
-        for epoch in range(self.num_epochs):
-            logs = distill_one_epoch(
-                self.query_encoder, self.key_encoder, 
-                targets, keys,
-                dataloader, loss, regularizer, optimizer, epoch, self.device, 
-                log_every_n_steps=5,
-            )
-            self.logs.extend(logs)
-
-        return self
-    
-    def embed(self, x: torch.Tensor) -> torch.Tensor:
-        """Embeds the input data with the query encoder."""
-        return self.query_encoder(x)
-    
-    def project(self, x: np.ndarray) -> np.ndarray:
-        """Projects an embedding to the output space with the key encoder."""
-        with torch.inference_mode():
-            x = torch.tensor(x, device=self.device, dtype=torch.float32)
-            return self.key_encoder(x).detach().cpu().numpy()
-
-    def get_informative_groups(self, groups: Sequence[Sequence[int]], norm_threshold=0.1) -> Sequence[Sequence[int]]:
-        """Get the most informative feature groups.
-
-        Args:
-            groups (Sequence[Sequence[int]]): Sequence of sequences containing the feature groups.
-
-        Returns:
-            Sequence[Sequence[int]]: Sequence of sequences containing the most informative feature groups.
-        """
-        return [
-            group for group in groups if torch.linalg.vector_norm(self.query_encoder.weight[:, group]) > norm_threshold
-        ]    
+def validate_mask_area(image: Image.Image, mask: torch.Tensor | np.ndarray, min_area_ratio=0.01, max_area_ratio=0.6):
+    im_area = image.size[0] * image.size[1]
+    mask_area = mask.sum()
+    return max_area_ratio * im_area > mask_area > min_area_ratio * im_area
         
-class LocalRepresentationExplainer:
-    def __init__(self):
-        pass
-
-    def fit_instance(self):
-        pass
-
-    def generate_local_data(self):
-        pass
-
-    def explain_downstream(self):
-        pass
+# mask generator that returns a list of masks
+def get_sam_mask_generator(
+    model="facebook/sam-vit-huge", points_per_batch=64, device=None,
+    mask_validator=validate_mask_area,
+    **pipeline_kwargs
+):
+    if device is None:
+        raise ValueError("device must be specified")
+    # see available args at https://github.com/huggingface/transformers/blob/main/src/transformers/pipelines/mask_generation.py#L94
+    # see args value at https://github.com/facebookresearch/segment-anything/blob/dca509fe793f601edb92606367a655c15ac00fdf/segment_anything/automatic_mask_generator.py#L36
+    generator = pipeline("mask-generation", 
+        model=model, 
+        points_per_batch=points_per_batch,
+        pred_iou_thresh=0.88,
+        stability_score_thresh=0.95,
+        stability_score_offset=1,
+        crops_n_layers=0,
+        crops_nms_thresh=0.7,
+        crop_overlap_ratio=512 / 1500,
+        crop_n_points_downscale_factor=1,
+        device=device, 
+        **pipeline_kwargs
+    )
+    def gen_fn(images: list[Image.Image], **kwargs):
+        batch = generator(images, **kwargs)
+        masks = [[
+                mask for mask in outputs['masks'] if mask_validator(image, mask)
+            ]
+            for image, outputs in zip(images, batch)
+        ]
+        return masks
+    return gen_fn
